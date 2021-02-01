841f05bc271e5130370f3849ee2b43a6fc03dc91
FabiolaSaga <saga.fabiola@gmail.com>
2021-01-31 19:36:10 -0600

    Added outlet for background view, created method to set the gradient background color

--- labs-ios-starter/Search/SearchViewController.swift
+++ labs-ios-starter/Search/SearchViewController.swift
@@ -9,22 +9,27 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
+    
+    // MARK: Outlets
+    @IBOutlet weak var backgroundGradient: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setGradientBackgroundColor()
         // Do any additional setup after loading the view.
     }
+ 
+    // MARK: - Background gradient
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    /// Sets the gradient colors for the background view
+    func setGradientBackgroundColor() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [UIColor(named: "LightBlue")!.cgColor, UIColor(named: "MedBlue")!.cgColor ]
+        gradientLayer.shouldRasterize = true
+        backgroundGradient.layer.addSublayer(gradientLayer)
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
     }
-    */
 
 }
