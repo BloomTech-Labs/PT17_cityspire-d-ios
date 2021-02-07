02ba06c20e5598b9c67390fdf8f4de1a80e16913
Jarren <jarrenmcampos@gmail.com>
2021-02-07 11:22:07 -0800

    added collection view protocols for data source, delegate, and flow

--- labs-ios-starter/Favorites/FavoritesViewController.swift
+++ labs-ios-starter/Favorites/FavoritesViewController.swift
@@ -9,10 +9,26 @@
 import UIKit
 
 class FavoritesViewController: UIViewController {
-
+    
+    @IBOutlet var colletionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+}
 
+extension FavoritesViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: view.frame.height / 3)
+        }
+    
 }
