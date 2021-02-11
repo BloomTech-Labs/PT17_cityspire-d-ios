983eaee4cf59dff3fcef313fd72241c9e8a1d422
Jarren <jarrenmcampos@gmail.com>
2021-02-10 21:21:34 -0800

    added Alert.swift file

---
+++ labs-ios-starter/Model/Alert.swift
@@ -0,0 +1,20 @@
+//
+//  Alert.swift
+//  labs-ios-starter
+//
+//  Created by Jarren Campos on 2/10/21.
+//  Copyright © 2021 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+struct Alert {
+    static func showBasicAlert(on vc: UIViewController, with title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        DispatchQueue.main.async {
+            vc.present(alert, animated: true)
+        }
+    }
+}
