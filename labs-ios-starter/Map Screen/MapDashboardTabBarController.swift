5c126d1e41c01ab633548c08bf15158ed984702d
Aaron <AJ_Ton2020@protonmail.com>
2021-03-25 21:27:35 -0500

    Snapped in IBOutlets and IBActions

--- labs-ios-starter/Map Screen/MapDashboardTabBarController.swift
+++ labs-ios-starter/Map Screen/MapDashboardTabBarController.swift
@@ -2,19 +2,53 @@
 //  MapDashboardTabBarController.swift
 //  labs-ios-starter
 //
-//  Created by Joe on 3/25/21.
+//  Created by AAron on 3/25/21.
 //  Copyright © 2021 Spencer Curtis. All rights reserved.
 //
-
+import CoreData
+import MapKit
 import UIKit
 
+/// Class controls MapView and Dashboard. User is able to view Favorite Cities in Dashboard
 class MapDashboardTabBarController: UITabBarController {
+    
+    // Context for CoreData
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var employmentLabel: UILabel!
+    @IBOutlet weak var averageAgeLabel: UILabel!
+    @IBOutlet weak var livabilityLabel: UILabel!
+    @IBOutlet weak var rentalLabel: UILabel!
+    @IBOutlet weak var walkabilityLabel: UILabel!
+    @IBOutlet weak var averageIncomeLabel: UILabel!
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var favoriteButton: UIButton!
+    @IBOutlet weak var cityNameLabel: UILabel!
+    
+    // MARK: - Properties
+
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updateViews()
     }
+    
+    // MARK: - Actions
+    @IBAction func searchButtonPressed(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    @IBAction func favoriteButtonPressed(_ sender: Any) {
+        favoriteButton.currentImage == UIImage(named: "FavoriteButton") ? favoriteButton.setImage(UIImage(named: "UncheckedHeart"), for: .normal) : favoriteButton.setImage(UIImage(named: "FavoriteButton"), for: .normal)
+    }
+    
+    
+    // MARK: - Methods
+    func updateViews() {
+        
+    }
+    
     
 
     /*
