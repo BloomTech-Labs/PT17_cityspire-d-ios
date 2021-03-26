209702d47f2d9cea44000c6aac7acffde11a3386
Aaron <AJ_Ton2020@protonmail.com>
2021-03-25 20:55:37 -0500

    Created New Map-Dashboard Files. Created Constraints for Dashboard.

---
+++ labs-ios-starter/Map Screen/MapDashboardTabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  MapDashboardTabBarController.swift
+//  labs-ios-starter
+//
+//  Created by Joe on 3/25/21.
+//  Copyright © 2021 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+
+class MapDashboardTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
